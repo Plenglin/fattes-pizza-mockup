e2d023a9b15f08030cc9b1ee913357fbd413eed8
Astrid Yu <astrid@astrid.tech>
2020-10-24 13:47:52 -0700

    Add the topping table

--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,11 @@
-import { Container } from '@material-ui/core';
+import { Container, Paper, Table, TableContainer, TableHead, TableBody, TableCell, TableRow } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
-import { fattesFavorites, salads, sides, subs } from './menu-data';
+import { fattesFavorites, salads, sides, subs, buildYourOwnPrices, toppings, doubleExtraToppings } from './menu-data';
 import { useStyles } from './util';
 
 function Price({data}) {
@@ -47,7 +47,7 @@ function MenuCategory({title, items}) {
     <>
       <Container className={classes.cardGrid} maxWidth="md">
         <Grid>
-          <Typography variant="h4" component="h4">{title}</Typography>
+          <Typography gutterBottom variant="h4" component="h4">{title}</Typography>
         </Grid>
         <Grid container spacing={4}>
           {items.map((item) => (
@@ -61,9 +61,73 @@ function MenuCategory({title, items}) {
   )
 }
 
+function BuildYourOwnCategory() {
+  const classes = useStyles();
+  return (
+    <>
+      <Container className={classes.cardGrid} maxWidth="md">
+        <Grid>
+          <Typography gutterBottom variant="h4" component="h4">Build Your Own Pizza!</Typography>
+        </Grid>
+        <Grid>
+          <Paper className={classes.paper}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6}>
+                <TableContainer>
+                  <Table className={classes.table} size="small">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell># of Toppings</TableCell>
+                        <TableCell>Small</TableCell>
+                        <TableCell>Medium</TableCell>
+                        <TableCell>Large</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {
+                        buildYourOwnPrices.map(([sm, md, lg], n) => (
+                          <TableRow>
+                            <TableCell>{n}</TableCell>
+                            <TableCell>{sm / 100}</TableCell>
+                            <TableCell>{md / 100}</TableCell>
+                            <TableCell>{lg / 100}</TableCell>
+                          </TableRow>
+                        ))
+                      }
+                      <TableRow>
+                        <TableCell>Double/Extra</TableCell>
+                        <TableCell>{doubleExtraToppings[0] / 100}</TableCell>
+                        <TableCell>{doubleExtraToppings[1] / 100}</TableCell>
+                        <TableCell>{doubleExtraToppings[2] / 100}</TableCell>
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <Typography gutterBottom variant="h6" component="h6">Select from the following toppings:</Typography>
+                <Grid grid container>
+                  {
+                    toppings.map(({name}) => (
+                      <Grid xs={4} md={3}>
+                        <Typography variant="body2" component="p">{name}</Typography>
+                      </Grid>
+                    ))
+                  }
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Container>
+    </>
+  )
+}
+
 export function Menu() {
   return (
     <>
+      <BuildYourOwnCategory/>
       <MenuCategory title="Fatte's Favorites" items={fattesFavorites}/>
       <MenuCategory title="Salads" items={salads}/>
       <MenuCategory title="Sides" items={sides}/>
