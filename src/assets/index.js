3393f413d33a2bc25ce1719c8829d5208dde934d
Astrid Yu <astrid@astrid.tech>
2020-10-24 12:36:56 -0700

    Add images to menu items, categorize menu

---
+++ src/assets/index.js
@@ -0,0 +1,15 @@
+import antipastoSalad from "./antipasto-salad.jpg";
+import buffaloWings from './buffalo-wings.jpg';
+import chickenCombo from './chicken-combo.jpg';
+import fattesSpecial from './fattes-special.jpg';
+import stickyFingers from './sticky-fingers.jpg';
+import veggiePizza from './veggie-pizza.jpg';
+
+export {
+    antipastoSalad,
+    buffaloWings,
+    chickenCombo,
+    fattesSpecial,
+    stickyFingers,
+    veggiePizza
+}
